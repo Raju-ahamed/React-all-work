9939755f3ad3876f295dd530caebc93428a3e223
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-09 23:34:11 +0600

    emibi

---
+++ src/Component/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            This Register
+        </div>
+    );
+};
+
+export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import Root from './layout/Root/Root.jsx';
+import Home from './Component/Home/Home.jsx';
+import Login from './Component/Login/Login.jsx';
+import Register from './Component/Register/Register.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    Component:Root,
+    children:[
+      {
+        index:true,
+        Component:Home
+      },
+      {path:"login",Component:Login},
+      {path:"register",Component:Register}
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
