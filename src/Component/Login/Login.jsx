784c06c15bdbdc3426fe04af05a555337b4d5b5e
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-11 15:28:10 +0600

    fiels

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-            This is Login
-            
+        <div className="card bg-base-100 w-full mx-auto mt-10 max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <h1 className="text-5xl font-bold">Login now!</h1>
+                <form className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>Create an account? <Link to="/register"><span className='underline text-blue-500'>Register</span></Link></p>
+            </div>
         </div>
     );
 };
